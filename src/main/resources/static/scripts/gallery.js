853c10a1aeec79ad6cc8dafe3337c5122e86d881
Daniel Lovell <daniellovell17@gmail.com>
2023-11-18 15:53:49 +0000

    Add more gallery images

--- src/main/resources/static/scripts/gallery.js
+++ src/main/resources/static/scripts/gallery.js
@@ -1,3 +1,36 @@
+// SHUFFLE IMAGES
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// get list of all gallery image paths
+let images = []
+let numImages = 27
+for (let i = 1; i <= numImages; i++) {
+    let imageNum = i > 9 ? "" + i : "0" + i;
+    images[i-1] = "/images/gallery/gallery" + imageNum + ".jpg";
+}
+
+// shuffle gallery image paths
+shuffle(images);
+
+// insert images into page
+let gallery = document.querySelector(".large-img-gallery");
+for (let i = 0; i < numImages; i++) {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    div.classList.add("img-container")
+    img.src = images[i];
+    div.appendChild(img);
+    gallery.appendChild(div);
+}
+
+
+
 // IMAGE MODAL
 
 let galleryImages = document.querySelectorAll(".large-img-gallery > .img-container > img");
