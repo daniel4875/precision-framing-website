dcd58cd448336b04f7a9ed32a6eb06fbeb9caefc
Daniel Lovell <daniellovell17@gmail.com>
2023-11-18 14:12:51 +0000

    Clean error controller and fix link in home page

--- src/main/java/uk/co/precisionframingyate/website/MyErrorController.java
+++ src/main/java/uk/co/precisionframingyate/website/MyErrorController.java
@@ -1,26 +1,14 @@
 package uk.co.precisionframingyate.website;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MyErrorController implements ErrorController {
 
-    @Autowired
-    ResourceLoader loader;
-
     @GetMapping("/error")
-    public ResponseEntity<Resource> errorPage() {
-        Resource htmlFile = loader.getResource("classpath:static/error.html");
-        return ResponseEntity
-                .status(404)
-                .header(HttpHeaders.CONTENT_TYPE, "text/html")
-                .body(htmlFile);
+    public String errorPage() {
+        return "error.html";
     }
 }
